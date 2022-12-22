672810afa40cd4516b58c62fc3ce753f621e8904
John Wason <wason@wasontech.com>
2022-12-22 18:04:10 -0500

    Fill in main to start scanner and RR service

--- src/artec_scanner_robotraconteur_driver.cpp
+++ src/artec_scanner_robotraconteur_driver.cpp
@@ -1,4 +1,6 @@
+#include "robotraconteur_generated.h"
 #include <RobotRaconteur.h>
+#include <RobotRaconteurCompanion/StdRobDef/StdRobDefAll.h>
 #include "artec_scanner_impl.h"
 
 #include <artec/sdk/capturing/IScanner.h>
@@ -20,13 +22,48 @@ using asdk::TArrayRef;
 
 namespace RR = RobotRaconteur;
 
-namespace artec_scanner_robotraconteur_driver
-{
-
-}
+using namespace artec_scanner_robotraconteur_driver;
 
 int main(int argc, char* argv[])
 {
 
+    asdk::setOutputLevel( asdk::VerboseLevel_Trace );
+    asdk::ErrorCode ec = asdk::ErrorCode_OK;
+    TRef<asdk::IArrayScannerId> scannersList;
+    std::cerr << "Enumerating scanners... " << std::endl;
+    ec = asdk::enumerateScanners( &scannersList );
+    if( ec != asdk::ErrorCode_OK )
+    {
+        std::cerr << "Enumerating scanners failed" << std::endl;
+        return 1;
+    }
+    int scanner_count = scannersList->getSize();
+    if( scanner_count == 0 )
+    {
+        std::cerr << "No scanners found" << std::endl;
+        return 3;
+    }
+    const asdk::ScannerId* idArray = scannersList->getPointer();
+    const asdk::ScannerId& defaultScanner = idArray[0]; // just take the first available scanner
+    std::wcerr 
+        << L"Connecting to " << asdk::getScannerTypeName( defaultScanner.type ) 
+        << L" scanner " << defaultScanner.serial << L"... "
+    ;
+    TRef<asdk::IScanner> scanner;
+    ec = asdk::createScanner( &scanner, &defaultScanner );
+    if( ec != asdk::ErrorCode_OK )
+    {
+        std::cerr << "Create scanner failed" << std::endl;
+        return 2;
+    }
+
+    auto scanner_impl = RR_MAKE_SHARED<ArtecScannerImpl>();
+    scanner_impl->Init(scanner);
+    
+    RR::ServerNodeSetup node_setup(ROBOTRACONTEUR_SERVICE_TYPES, "experimental.artec_scanner", 64238);
+    RR::RobotRaconteurNode::s()->RegisterService("scanner", "experimental.artec_scanner", scanner_impl);
+
+    std::cout << "Press enter to quit..." << std::endl;
+    getchar();
 
 }
